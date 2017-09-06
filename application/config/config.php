73098aef6a73f2df7b839f63995312ba172ac3cc
dangorne <30405168+dangorne@users.noreply.github.com>
2017-09-06 18:25:49 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://192.168.5.61/clerk';
-$config['base_url'] = 'http://localhost/clerk';
-// $config['base_url'] = 'http://192.168.1.13/clerk';
+//$config['base_url'] = 'http://localhost/clerk';
+$config['base_url'] = 'https://dantestapp1.herokuapp.com/';
 //$config['base_url'] = 'http://192.168.43.99/clerk';
 
 /*
