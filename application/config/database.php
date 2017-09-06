552fd9f97f97a21f6675d1fd4a91c10021cbdade
dangorne <30405168+dangorne@users.noreply.github.com>
2017-09-06 18:13:31 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 
-mysql://b180ecb9a6f662:9f35cfa2@us-cdbr-iron-east-05.cleardb.net/heroku_3541019aea969af?reconnect=true
+//mysql://b180ecb9a6f662:9f35cfa2@us-cdbr-iron-east-05.cleardb.net/heroku_3541019aea969af?reconnect=true
 
 $db['default'] = array(
 	'dsn'	=> '',
@@ -14,7 +14,7 @@ $db['default'] = array(
 	'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
 	'username' => 'b180ecb9a6f662',
 	'password' => '9f35cfa2',
-	'database' => 'smartq',
+	'database' => 'heroku_3541019aea969af',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
